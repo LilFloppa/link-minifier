4e3a31232ab5e946119f31a1f72a0a0b3f1fbf2c
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-27 23:56:47 +0700

    Add LinkService

---
+++ LinkMinifier.Services/Interfaces/ILinkService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace LinkMinifier.Services.Interfaces
+{
+    public interface ILinkService
+    {
+        Task MinifyLink(string link);
+        Task<string> GetOriginalLink(string minifiedLink);
+    }
+}
---
+++ LinkMinifier.Services/LinkService.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using LinkMinifier.Services.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinkMinifier.Services
+{
+    public class LinkService : ILinkService
+    {
+
+        private readonly ApplicationContext context;
+        public LinkService(ApplicationContext context) => this.context = context;
+
+        public async Task<string> GetOriginalLink(string minifiedLink)
+        {
+            await Task.Delay(2000);
+            return "https://www.google.com/";
+        }
+
+        public Task MinifyLink(string link)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
