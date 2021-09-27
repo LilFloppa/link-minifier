e8f2c8c684395049b2fc4acedb5654e63ca02a52
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-27 23:58:22 +0700

    Add services and controller for WebAPI

---
+++ LinkMinifier.WebAPI/Controllers/LinkController.cs
@@ -0,0 +1,26 @@
+﻿using LinkMinifier.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LinkMinifier.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class LinkController : ControllerBase
+    {
+        private readonly ILinkService linkService;
+        public LinkController(ILinkService linkService) => this.linkService = linkService;
+
+        [HttpGet]
+        [Route("{minified}")]
+        public async Task<IActionResult> Get(string minified)
+        {
+            string original = await linkService.GetOriginalLink("Hello");
+
+            if (original != string.Empty)
+                return Redirect(original);
+
+            return NotFound();
+        }
+    }
+}
--- LinkMinifier.WebAPI/Startup.cs
+++ LinkMinifier.WebAPI/Startup.cs
@@ -1,16 +1,11 @@
+using Data;
+using LinkMinifier.Services;
+using LinkMinifier.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LinkMinifier.WebAPI
 {
@@ -26,12 +21,9 @@ namespace LinkMinifier.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "LinkMinifier.WebAPI", Version = "v1" });
-            });
+            services.AddScoped<ILinkService, LinkService>();
+            services.AddDbContext<ApplicationContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +32,6 @@ namespace LinkMinifier.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LinkMinifier.WebAPI v1"));
             }
 
             app.UseHttpsRedirection();
