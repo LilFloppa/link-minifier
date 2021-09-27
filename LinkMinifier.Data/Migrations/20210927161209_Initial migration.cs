2130f3d64d22ff77e89feb0f73e57cffb405753b
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-27 23:15:57 +0700

    Add Initial migration for SQLite database

---
+++ LinkMinifier.Data/Migrations/20210927161209_Initial migration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LinkMinfier.Data.Migrations
+{
+    public partial class Initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OriginalLink = table.Column<string>(type: "TEXT", nullable: true),
+                    MinifiedLink = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
--- LinkMinifier.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LinkMinifier.Services
-{
-    public class Class1
-    {
-    }
-}
