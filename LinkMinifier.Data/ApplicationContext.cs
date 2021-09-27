ac31d527f164269dbd353e7efbe945a38d2231c7
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-27 23:09:13 +0700

    Add basic project structure

---
+++ LinkMinifier.Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Link> Links { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=Link.db");
+        }
+    }
+}
---
+++ LinkMinifier.Data/Models/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+        public string OriginalLink { get; set; }
+        public string MinifiedLink { get; set; }
+    }
+}
