218ba57675f04cfc3421ddc6f3f09de8689de12f
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-28 14:28:05 +0700

    add WebUI interface

---
+++ LinkShortener.WebUI/Models/Link.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.WebUI.Models
+{
+    public class Link
+    {
+        public string OriginalLink { get; set; }
+        public string ShortenedLink { get; set; }
+    }
+}
