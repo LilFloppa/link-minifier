7e96f3bff7f4cfbacb67c668a03e52061efbe450
Никита Скрынник <nikita.nstu@gmail.com>
2021-09-28 15:00:19 +0700

    add form validation

--- LinkShortener.WebUI/Models/Link.cs
+++ LinkShortener.WebUI/Models/Link.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace LinkShortener.WebUI.Models
+﻿namespace LinkShortener.WebUI.Models
 {
     public class Link
     {
